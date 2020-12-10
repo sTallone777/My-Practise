5ce0704381dd5ed20f36c6a514c70899c3352102
ranbo <45021798+sTallone777@users.noreply.github.com>
2020-12-10 15:21:42 +0900

    Update main.go

--- leetcode_homework/t10_isMatch/main.go
+++ leetcode_homework/t10_isMatch/main.go
@@ -14,30 +14,47 @@ func isMatch(s string, p string) bool {
 	ret := true
 	ss := 0
 	ps := 0
+	si := 0
 
 	for ps < len(p) {
 		if (ps+1 < len(p)) && (p[ps+1] == '*') {
-			for ss < len(s) && compare(rune(s[ss]), rune(p[ps])) {
+			si = ss
+			for ss < len(s) && compare(s, p, ss, ps) {
 				ss++
 			}
 			ps += 2
+			if ss == len(s) && ps != len(p) && ss != si {
+				if strings.ContainsRune(p[ps:], '*') {
+					ss = ss - (strings.IndexRune(p[ps:], '*') - 1)
+				} else {
+					ss = ss - len(p[ps:])
+				}
+			}
 		} else {
-			ret = ret && compare(rune(s[ss]), rune(p[ps]))
+			ret = ret && compare(s, p, ss, ps)
 			ss++
 			ps++
 		}
+
 	}
 
-	if ss != len(s) {
+	if ss != len(s) || ps != len(p) {
 		return false
 	}
 
 	return ret
 }
 
-func compare(s rune, p rune) bool {
-	if p != '.' {
-		return p == s
+func compare(s string, p string, ss int, ps int) bool {
+	if ss >= len(s) {
+		if (ps+1 < len(p)) && (p[ps+1] == '*') {
+			return true
+		}
+		return false
+	}
+
+	if p[ps] != '.' {
+		return p[ps] == s[ss]
 	}
 	return true
 }

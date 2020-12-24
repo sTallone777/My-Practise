9cd7946d6416e593d34c2d4f0e9efab9047fe7f6
ranbo <45021798+sTallone777@users.noreply.github.com>
2020-12-24 14:59:11 +0900

    Update main.go

--- leetcode_homework/t94_inorderTraversal/main.go
+++ leetcode_homework/t94_inorderTraversal/main.go
@@ -12,6 +12,95 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//Get node value
+func (t *TreeNode) getValue() int {
+	return t.Val
+}
+
+//Set node value
+func (t *TreeNode) setValue(v int) {
+	t.Val = v
+}
+
+//Search node by value
+func (t *TreeNode) findNode(root *TreeNode, v int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.getValue() == v {
+		return root
+	}
+	node := t.findNode(root.Left, v)
+	if node != nil {
+		return node
+	}
+	return t.findNode(root.Right, v)
+}
+
+//Calculate tree height
+func (t *TreeNode) getTreeHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	lh := t.getTreeHeight(root.Left)
+	rh := t.getTreeHeight(root.Right)
+	if lh > rh {
+		return lh + 1
+	}
+	return rh + 1
+}
+
+//Print leafnode
+func (t *TreeNode) printLeafNode(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	if root.Left == nil && root.Right == nil {
+		fmt.Printf("%d\r\n", root.Val)
+	}
+	t.printLeafNode(root.Left)
+	t.printLeafNode(root.Right)
+}
+
+//Print treenode
+func (t *TreeNode) printTreeNode() {
+	if t == nil {
+		return
+	}
+	fmt.Printf("Node Value: %d\r\n", t.Val)
+	fmt.Printf("Left Node: %p\r\n", t.Left)
+	fmt.Printf("Right Node: %p\r\n", t.Right)
+}
+
+//pre-order traversal
+func preorderTraversal(root *TreeNode) []int {
+	retArr := []int{}
+	if root == nil {
+		return nil
+	}
+	retArr = append(retArr, root.Val)
+	retArr = append(retArr, preorderTraversal(root.Left)...)
+	retArr = append(retArr, preorderTraversal(root.Right)...)
+
+	return retArr
+}
+
+//post-order tarversal
+func postorderTraversal(root *TreeNode) []int {
+	retArr := []int{}
+	if root == nil {
+		return nil
+	}
+
+	retArr = append(retArr, postorderTraversal(root.Left)...)
+	retArr = append(retArr, postorderTraversal(root.Right)...)
+	retArr = append(retArr, root.Val)
+
+	return retArr
+}
+
+//in-order traversal
 func inorderTraversal(root *TreeNode) []int {
 	retArr := []int{}
 	if root == nil {
@@ -29,7 +118,12 @@ func inorderTraversal(root *TreeNode) []int {
 func createTree(lr string, d int, parent int) *TreeNode {
 	var a string
 
-	fmt.Printf("Please input the deep: %d %s node:\r\n", d, lr)
+	if lr == "root" {
+		fmt.Printf("Please input the deep: %d %s node of root:\r\n", d, lr)
+	} else {
+		fmt.Printf("Please input the deep: %d %s node of node: %d:\r\n", d, lr, parent)
+	}
+
 	fmt.Scanln(&a)
 	//Stop to creating the branch by typing '#'
 	if a == "#" {
@@ -60,6 +154,22 @@ func main() {
 	}
 	fmt.Println("Create node: root successed.")
 
-	middleTraversal := inorderTraversal(root)
-	fmt.Printf("%v", middleTraversal)
+	preorder := preorderTraversal(root)
+	fmt.Printf("Pre-order traversal: %v\r\n", preorder)
+
+	inorder := inorderTraversal(root)
+	fmt.Printf("In-order traversal: %v\r\n", inorder)
+
+	postorder := postorderTraversal(root)
+	fmt.Printf("Post-order traversal: %v\r\n", postorder)
+
+	node1 := root.findNode(root, 3)
+	node1.printTreeNode()
+	fmt.Printf("This tree's height is : %d\r\n", root.getTreeHeight(node1))
+	node2 := root.findNode(root, 5)
+	node2.printTreeNode()
+	fmt.Printf("This tree's height is : %d\r\n", root.getTreeHeight(node2))
+
+	fmt.Println("Print LeafNode: ")
+	root.printLeafNode(root)
 }

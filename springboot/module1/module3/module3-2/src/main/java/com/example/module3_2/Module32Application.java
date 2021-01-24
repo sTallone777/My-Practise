d8a46725885127f3ab5a494b0c884a979055849a
ranbo <wanglanbo123@icloud.com>
2021-01-24 21:03:51 +0900

    module

---
+++ springboot/module1/module3/module3-2/src/main/java/com/example/module3_2/Module32Application.java
@@ -0,0 +1,13 @@
+package com.example.module3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module32Application.class, args);
+	}
+
+}
